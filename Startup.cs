0f96c257cdf91c7dc8ac49e8f2473cc94a016946
Stuart Leeks <stuart@leeks.net>
2015-05-31 21:17:01 +0100

    Add project files

---
+++ Startup.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.DependencyInjection;
+
+namespace EmptyWeb
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+        }
+
+        // This method gets called by a runtime.
+        // Use this method to add services to the container
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+
+        // Configure is called after ConfigureServices is called.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseWelcomePage();
+        }
+    }
+}
