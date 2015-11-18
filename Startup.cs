6e910babd10d1492a5c25a5211548b06944b775c
Stuart Leeks <stuart@leeks.net>
2015-11-18 21:53:18 +0000

    Simplify startup class

--- Startup.cs
+++ Startup.cs
@@ -11,18 +11,6 @@ namespace EmptyWeb
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
-        {
-        }
-
-        // This method gets called by a runtime.
-        // Use this method to add services to the container
-        public void ConfigureServices(IServiceCollection services)
-        {
-            
-        }
-
-        // Configure is called after ConfigureServices is called.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseWelcomePage();
