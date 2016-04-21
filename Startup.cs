84d057a0516aff8ab592ab3cbec1d41840e5d67f
Stuart Leeks <stuart@leeks.net>
2016-04-21 21:15:41 +0100

    RC2 changes

--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 
 namespace EmptyWeb
 {
@@ -14,6 +12,19 @@ namespace EmptyWeb
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseWelcomePage();
+        }
+        
+        
+         // Entry point for the application.
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseDefaultHostingConfiguration(args)
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
     }
 }
